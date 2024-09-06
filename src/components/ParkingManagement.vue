4953d9b2c7e0dfe73a5237080e8815e62d25b1d1
Cyrille <cyrille.burtin@me.com>
2024-09-06 11:52:44 +0200

    [feat] - create ParkingManagment component

---
+++ src/components/ParkingManagement.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import ParkingLot from '../components/ParkingLot.vue';
+import { useParkingLots } from '../stores/parkingLots';
+
+const { lots } = useParkingLots();
+</script>
+
+<template>
+  <div class="flex gap-2 flex-wrap">
+    <ParkingLot v-for="{ isAvailable, id } in lots" :key="id" :id="id" :isAvailable="isAvailable" />
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { useParkingLots } from '../stores/parkingLots';
-import ParkingLot from '../components/ParkingLot.vue';
 import GetTicket from '../components/GetTicket.vue';
 import ExitParking from '../components/ExitParking.vue';
+import ParkingManagement from '@/components/ParkingManagement.vue';
 const { lots } = useParkingLots();
 </script>
 
@@ -10,15 +10,6 @@ const { lots } = useParkingLots();
   <div class="flex flex-col gap-16">
     <GetTicket :lots="lots" />
     <ExitParking />
-    <h2>Place disponibles</h2>
-    <br />
-    <div class="flex gap-2 flex-wrap">
-      <ParkingLot
-        v-for="{ isAvailable, id } in lots"
-        :key="id"
-        :id="id"
-        :isAvailable="isAvailable"
-      />
-    </div>
+    <ParkingManagement />
   </div>
 </template>
